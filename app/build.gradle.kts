8653715a719ec2af20af8244973251fe7a8ce144
Jokwanhee <jonotch1@naver.com>
2024-05-13 15:25:23 +0900

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.koreatech.kotrip_android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10000
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "kakao_native_app_key", getPropertyKey("kakao_native_app_key"))
